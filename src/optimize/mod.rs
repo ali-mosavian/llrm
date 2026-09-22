15c041baab8cb69b037ce695c9314abbbc491b03
Ali Mosavian <ali@42labs.ai>
2026-09-22 13:55:50 +0200

    feat(optimize): port loopclone

--- src/optimize/mod.rs
+++ src/optimize/mod.rs
@@ -5,6 +5,7 @@ pub(crate) mod edges;
 pub(crate) mod indvars;
 pub(crate) mod lcssa;
 pub(crate) mod lcssamerges;
+pub(crate) mod loopclone;
 pub(crate) mod loopsimplify;
 pub(crate) mod rotate;
 pub(crate) mod strength;
