393389fefaec5675e7dc9562a7221f4c62e2f642
Ali Mosavian <ali@42labs.ai>
2026-09-22 14:01:45 +0200

    feat(optimize): port ivshare and exitsink

--- src/optimize/mod.rs
+++ src/optimize/mod.rs
@@ -2,7 +2,9 @@
 
 pub(crate) mod cfg;
 pub(crate) mod edges;
+pub(crate) mod exitsink;
 pub(crate) mod indvars;
+pub(crate) mod ivshare;
 pub(crate) mod rotate;
 pub(crate) mod strength;
 pub(crate) mod transform;
